977a98ac6fa6c1cbbfa979f33859a9138c8c9f24
Katherine Stanley <11195226+katheris@users.noreply.github.com>
2022-11-22 12:12:59 +0000

    feat: Add files for 04_02 end
    Signed-off-by: Katherine Stanley <11195226+katheris@users.noreply.github.com>

---
+++ src/main/java/com/example/demo/EnvHealth.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Startup;
+
+@Startup
+@ApplicationScoped
+public class EnvHealth implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        String customEnv = System.getenv("MY_CUSTOM_ENV");
+        if (customEnv != null) {
+            return HealthCheckResponse.named("EnvCheck")
+                   .withData("present", true)
+                   .up()
+                   .build();
+        } else {
+            return HealthCheckResponse.named("EnvCheck")
+                   .withData("present", false)
+                   .down()
+                   .build();
+        }
+    }
+}
