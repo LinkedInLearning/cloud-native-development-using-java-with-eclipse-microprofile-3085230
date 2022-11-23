8878cf0a52fd02d5e5d867ff97d62ee28aa08133
Katherine Stanley <11195226+katheris@users.noreply.github.com>
2022-11-23 10:58:53 +0000

    feat: Add files for 06_03 end
    Signed-off-by: Katherine Stanley <11195226+katheris@users.noreply.github.com>

---
+++ app1/src/main/java/com/example/demo/HelloWorldResource.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
+@Path("/hello")
+public class HelloWorldResource {
+
+    @Timed(name = "HelloRequestTime",
+        absolute = true,
+        description = "Time taken to respond to hello world")
+    @Counted(name = "HelloRequestCount",
+        absolute = true,
+        description = "Number of times hello is called")
+    @GET
+    public Response sayHello() {
+        return Response.ok("Hello World").build();
+    }
+
+}
---
+++ app2/src/main/java/com/example/demo/LivenessCheck.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Liveness;
+
+@Liveness
+@ApplicationScoped
+public class LivenessCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.named("Liveness").up().build();
+    }
+}
